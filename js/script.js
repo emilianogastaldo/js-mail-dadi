b379a3503eb2704d1c846a8059523190b48c549a
emilianogastaldo <emiliano0303@gmail.com>
2023-12-21 16:17:52 +0100

    primo esercizio

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
 console.log('js ok')
+
+// ESERCIZIO 1
+
+//Recupero il bottone
+const playButton = document.getElementById('playbutton');
+
+//Recupero i due div dove scrivere i messaggi
+let message = document.getElementById('message');
+let players = document.getElementById('players')
+
+//Creo un bottone per gioacare
+playButton.addEventListener('click', function () {
+    //Creo i due numeri ramdomici tra 1 e 10
+    const playerNumber = Math.floor(Math.random() * 10) + 1;
+    const computerNumber = Math.floor(Math.random() * 10) + 1;
+
+    //Inizializzo il messaggio per il vincitore
+    let winner = '';
+
+    //Controllo chi vince o pareggia
+    if (playerNumber > computerNumber) {
+        winner = 'Vince il Giocatore';
+    } else if (playerNumber < computerNumber) {
+        winner = 'Vince il Computer';
+    } else {
+        winner = 'Giocatore e Computer hanno pareggiato';
+    }
+
+    //Stampo a schermo i messaggi
+    players.innerHTML = `Il <u>Giocatore</u> ha fatto <strong>${playerNumber}</strong>, il <u>Computer</u> ha fatto <strong>${computerNumber}</strong>`;
+    message.innerText = winner;
+})
